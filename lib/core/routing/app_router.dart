dffaf082a3eae625aeaf30c55b87d34f1a683f89
Ibrahim Salem <ibrahim.deep.dev@gmail.com>
2024-12-10 14:31:04 +0200

    - Added dependency injection - Added App Regex - integrated with login EP (End-Point) - Added Login repo & cubit - Added Login states with validation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,11 +1,15 @@
+import 'package:doctors_app/core/di/dependency_injection.dart';
 import 'package:doctors_app/core/routing/routes.dart';
+import 'package:doctors_app/features/home/ui/home_screen.dart';
+import 'package:doctors_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:doctors_app/features/login/ui/login_screen.dart';
 import 'package:doctors_app/features/on_boarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
-    //this argument to be passed in any screen like this 
+    //this argument to be passed in any screen like this
     // final argument = settings.arguments;
 
     switch (settings.name) {
@@ -16,7 +20,15 @@ class AppRouter {
 
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
+
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
         );
 
       default:
